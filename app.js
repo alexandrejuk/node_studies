0a9601e7957bfc11f1d48f2a4a6786c58eb94074
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-13 09:43:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const port     =     3003;
-const express  =     require('express');
-const app      =     express();
+const port = 3003;
+const express = require('express');
+
+
+const router = require('./controllers/clientsController');
+
+const app = express();
+
+
 
 app.listen(port);
 console.log(`Application it is in ${port}`);
