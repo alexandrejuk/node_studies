56888d71683b85f5fb3b14f618c2ed89cb416470
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-15 10:55:02 -0300

    fixing bugs

--- models/clients.js
+++ models/clients.js
@@ -5,6 +5,8 @@ const _db = require('./_db');
 const clientSchema = mongoose.Schema( { 
     name: String,
     phone: Number
- })
+ }, {
+    versionKey: false 
+});
 
 module.exports = mongoose.model('clients', clientSchema);
