a1136e2b8da2a05a061a459f6c2988e0cc67b4c2
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-15 13:28:23 -0300

    adding routes to clientsRoute.js

--- routes/clientsRoute.js
+++ routes/clientsRoute.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const clientController = require("./../controllers/clientsController");
 
 
-router.get("/clients", clientController.list);
+router.get("/clients", clientController.getAllClients);
+router.get("/clients/:id", clientController.getOneClient);
+router.delete("/clients/:id", clientController.deleteOneClient);
+
+
 
 module.exports = router;
