2aa1c35fff023ac071bd587f9dca55fcb2c4f362
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-15 10:56:38 -0300

    importing the routes to the app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const port = 3003;
 const express = require('express');
-
-
-const router = require('./controllers/clientsController');
+const routeClient = require('./routes/clientsRoute')
 
 const app = express();
-
-
+app.use('/api', routeClient);
 
 app.listen(port);
 console.log(`Application it is in ${port}`);
