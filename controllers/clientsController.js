6a97bb4783efa08e52efd6381e878bb22f594344
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-15 13:27:04 -0300

    defining a client's controls, delete and get

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -1,10 +1,40 @@
 const clients = require('../models/clients');
 
-exports.list = function(req, resp, next) {
+
+let creationClient = new clients ({
+    name: "Alexandre dos Santos Soares",
+    phone: 98765421
+});
+creationClient.save(function(){
+    console.log(creationClient)
+})
+
+
+exports.getAllClients = function(req, resp, next) {
     clients.find().then(foundClients => {
 
         resp.status(200)
         .json(foundClients);
+
+    })
+}
+
+exports.getOneClient = function(req, resp, next) {
+    console.log(req.params.id);
+    clients.findById(req.params.id).then(foundOneClients => {
+
+        resp.status(200)
+        .json(foundOneClients);
+
+    })
+}
+
+exports.deleteOneClient = function(req, resp, next) {
+    console.log(req.params.id);
+    clients.remove({_id: req.params.id}).then(deleteOneClients => {
+
+        resp.status(200)
+        .json({message: 'Client was removed!', deleteOneClient});
 
     })
 }
