405da5dd1c19d45a7559b83f94ad321dd7b96b26
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-17 22:11:14 -0300

    Update models

--- models/clients.js
+++ models/clients.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const _db = require('./_db');
+const Schema = mongoose.Schema;
 
-
-const clientSchema = mongoose.Schema( { 
+const clientSchema = new Schema({ 
     name: String,
     phone: Number
  }, {
     versionKey: false 
 });
 
-module.exports = mongoose.model('clients', clientSchema);
+module.exports = _db.model('clients', clientSchema);
