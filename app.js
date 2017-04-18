509f4016a089af20fa669120a37827634600f5b1
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-18 17:34:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3003;
-const clientRoute = require('./routes/clientsRoute')
+const port = process.env.PORT || 3003;
+const clientRoute = require('./routes/clientsRoute');
 
 app.use(bodyParser.json());
 app.use("/api", clientRoute);
@@ -10,9 +10,8 @@ app.use("/api", clientRoute);
 
 
 
-
-
-app.listen(port);
-console.log(`Application it is in ${port}`);
+app.listen(port, ()=>{
+    console.log(`Application it is in ${port}`);
+});
 
 module.exports = app;
