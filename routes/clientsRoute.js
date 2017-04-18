7914c7250e91937a7604a17b70142f0493941705
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-17 22:12:24 -0300

    added the post and put routes

--- routes/clientsRoute.js
+++ routes/clientsRoute.js
@@ -5,7 +5,8 @@ const clientController = require("./../controllers/clientsController");
 router.get("/clients", clientController.getAllClients);
 router.get("/clients/:id", clientController.getOneClient);
 router.delete("/clients/:id", clientController.deleteOneClient);
-
+router.post("/clients", clientController.postClients);
+router.put("/clients/:id", clientController.putOnClient);
 
 
 module.exports = router;
