d5acbdd77dd732e8fa27a521b68d32d56dbc861a
alexandrejuk <ale_santos.soares@hotmail.com>
2017-04-17 22:09:05 -0300

    added the body-parser

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const port = 3003;
 const clientRoute = require('./routes/clientsRoute')
 
+app.use(bodyParser.json());
 app.use("/api", clientRoute);
+
+
+
+
+
 
 app.listen(port);
 console.log(`Application it is in ${port}`);
